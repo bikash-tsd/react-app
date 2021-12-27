3c46ffdf219c36cc2b3cdf887c97784c644f32e5
bikash-tsd <bdash@tsdweb.com>
2021-12-27 16:51:24 +0530

    list todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
-
-function App() {
+const  App:React.FC =()=> {
+  const todos = [
+    {id:'t1',text:'finish the course'}
+]
   return (
     <div className="App">
-     APp
+     <TodoList items={todos}/>
     </div>
   );
 }
